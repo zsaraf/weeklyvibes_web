06612c61bdb0f5378639497ea89233605534f1e6
Raymond Kennedy <franzwarning@gmail.com>
2016-04-25 12:36:31 -0700

    Fonts

--- client/app/js/components/EventPlaylist.js
+++ client/app/js/components/EventPlaylist.js
@@ -2,19 +2,31 @@
 
 import React from 'react';
 
-
 class EventPlaylistNode extends React.Component {
 
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     render() {
 
+        console.log(this.props.event);
+
         return (
             <div className='event-playlist-node'>
-                <div className='name'>
-                    {this.props.event.name}
+                <div className='left-content'>
+                    <div className='artist-img-wrapper'>
+                        <img src={this.props.event.eventArtists[0].artist.imgSrc} />
+
+                    </div>
+                </div>
+                <div className='right-content'>
+                    <div className='name'>
+                        {this.props.event.name}
+                    </div>
+                    <div className='for-date'>
+                        {this.props.event.startDt}
+                    </div>
                 </div>
             </div>
         );
